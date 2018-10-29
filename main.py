5aaadb1032e3976799ad94bbf554b0fa1e5a59b3
ch-tseng <ch.tseng@sunplusit.com>
2018-10-29 18:49:07 +0800

    update

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import serial
 import cv2
 import imutils
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plot
 
 Serial = serial.Serial("/dev/ttyS0", 9600, timeout= 0.5 )
 
-plotLength = 200
+plotLength = 8
 
 tList = []
 hList = []
@@ -45,27 +45,19 @@ def readSerial():
 
     return dataString
 
-figure = matplotlib.pyplot.figure(num=None, figsize=(16, 4), dpi=80, facecolor='w', edgecolor='k')
-plot   = figure.add_subplot ( 111 )
 
-def fig2data ( fig ):
-    """
-    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
-    @param fig a matplotlib figure
-    @return a numpy 3D array of RGBA values
-    """
-    # draw the renderer
-    fig.canvas.draw ( )
- 
-    # Get the RGBA buffer from the figure
-    w,h = fig.canvas.get_width_height()
-    buf = np.fromstring ( fig.canvas.tostring_argb(), dtype=np.uint8 )
-    buf.shape = ( w, h,4 )
- 
-    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
-    buf = np.roll ( buf, 3, axis = 2 )
-    return buf
+#plot.figure()
+figure = plot.figure(num=None, figsize=(18, 8), dpi=80, facecolor='w', edgecolor='k')
+ax_t = figure.add_subplot(2,2,1)
+ax_t.set_title("Temperature (C)")
+ax_h = figure.add_subplot(2,2,2)
+ax_h.set_title("Humandity (%)")
+ax_l = figure.add_subplot(2,2,3)
+ax_l.set_title("Lightness (degree)")
+ax_w = figure.add_subplot(2,2,4)
+ax_w.set_title("Water (degree)")
 
+i = 0
 while True:
     data = readSerial()
 
@@ -73,31 +65,42 @@ while True:
         dataList = data.split(",")
         for dataValue in dataList:
             if(dataValue!=""):
-                print(dataValue)
                 now = datetime.now()
                 dataTime = now.strftime("%H:%M:%S")
                 sType, sValue = dataValue.split(":")
-                print("Split:", sType ,sValue )
+                #print(sType, sValue)
 
                 if(sType=="T"):
                     tList = inputData(tList, float(sValue), plotLength)
                     timeList_t = inputData(timeList_t, dataTime, plotLength)
+
                 elif(sType=="H"):
                     hList = inputData(hList, float(sValue), plotLength)
                     timeList_h = inputData(timeList_h, dataTime, plotLength)
                 elif(sType=="L"):
-                    lList = inputData(hList, int(sValue), plotLength)
+                    lList = inputData(lList, int(sValue), plotLength)
                     timeList_l = inputData(timeList_l, dataTime, plotLength)
                 elif(sType=="W"):
-                    wList = inputData(hList, int(sValue), plotLength)
+                    wList = inputData(wList, int(sValue), plotLength)
                     timeList_w = inputData(timeList_w, dataTime, plotLength)
 
                 # draw a cardinal sine plot
                 x = np.array (timeList_t )
                 y = np.array (tList)
-                print(x)
-                print(y)
-                plot.plot ( x, y )
+                ax_t.plot ( x, y )
+
+                x = np.array (timeList_h )
+                y = np.array (hList)
+                ax_h.plot ( x, y )
+
+                x = np.array (timeList_l )
+                y = np.array (lList)
+                ax_l.plot ( x, y )
+
+                x = np.array (timeList_w )
+                y = np.array (wList)
+                ax_w.plot ( x, y )
+
 
                 figure.canvas.draw()
                 # convert canvas to image
@@ -105,7 +108,10 @@ while True:
                 img  = img.reshape(figure.canvas.get_width_height()[::-1] + (3,))
                 # img is rgb, convert to opencv's default bgr
                 img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+                print("LEN",len(wList))
 
                 #matplotlib.pyplot.show()
                 cv2.imshow("TEST", img)
                 cv2.waitKey(1)
+
+    i += 1
